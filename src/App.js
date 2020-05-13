e0037bdaabfb25529d224920afaf90699b7663c0
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-05-12 22:00:57 -0400

    still need returned data and one more error message to display to screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Form from './Components/Form.js';
+import React from "react";
+import "./App.css";
+import Form from "./Components/Form.js";
 
 function App() {
   return (
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,23 @@
 import React, { useState } from "react";
 import * as yup from "yup";
 import axios from "axios";
+import { validate } from "json-schema";
+
+const formSchema = yup.object().shape({
+    name: yup
+        .string()
+        .required("Name is a required field"),
+    email: yup 
+        .string()
+        .email("A valid email address is required")
+        .required("An email adress must be added"),
+    password: yup 
+        .string()
+        .required("A password is required"),
+    terms: yup 
+        .boolean()
+        .oneOf([true], "Agree or else!")
+});
 
 const Form = () => {
     const [formState, setFormState] = useState({
@@ -13,13 +30,56 @@ const Form = () => {
     const [errorState, setErrorState] = useState({
         name: "",
         email: "",
-        password: "",
+        password: "Where's the password?",
         terms: ""
     });
 
+    const validate = e => {
+        let value = 
+            e.target.type === "checkbox" ? e.target.chekced :
+            e.target.value;
+        yup 
+            .reach(formSchema, e.target.name)
+            .validate(value)
+            .then(valid => {
+                setErrorState({
+                    ...errorState, [e.target.name]: ""
+                });
+            })
+            .catch(err => {
+                setErrorState({
+                    ...errorState, [e.target.name]: err.errors[0]
+                });
+            });
+    };
+
+    const inputChange = e => {
+        e.persist();
+
+        validate(e);
+        let value = 
+            e.target.type === "checkbox" ? e.target.checked : 
+            e.target.value;
+        setFormState({ ...formState, [e.target.name]: value });
+    }
+
+    const clearUserForm = () => {
+        setFormState({ name: "", email: "", password: "", terms: false });
+    };
+
+    const formSubmit = e => {
+        e.preventDefault();
+        console.log("form submission complete");
+        axios
+            .post(' https://reqres.in/api/users', formState)
+            .then(response => console.log(response))
+            .catch(err => console.log(err));
+        clearUserForm();
+    };
+
     return (
         <div>
-            <form>
+            <form onSubmit={formSubmit}>
                 <label htmlFor="name">
                     Name
                     <input
@@ -27,8 +87,46 @@ const Form = () => {
                         name="name"
                         id="name"
                         value={formState.name}
+                        onChange={inputChange}
                     />
                 </label>
+                <label htmlFor="email">
+                    Email 
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={formState.email}
+                        onChange={inputChange}
+                    /> 
+                    {errorState.email.length > 0 ? (
+                        <p className="error">{errorState.email}</p>
+                    ) : null}
+                </label>
+                <label htmlFor="password">
+                    Password 
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={formState.password}
+                        onChange={inputChange}
+                    />
+                </label>
+                <label htmlFor="terms">
+                    <input  
+                        type="checkbox"
+                        name="terms"
+                        id="terms"
+                        checked={formState.terms}
+                        onChange={inputChange}
+                    />
+                    Terms of Service
+                    {errorState.terms.length > 0 ? (
+                        <p className="error">{errorState.terms}</p>
+                    ) : null }
+                </label>
+                <button>Onboard thyself!</button>
             </form>
         </div>
     )
