9c51ca6000341680d0a50c6f1d4ff49f7874a4af
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-05-14 20:41:48 -0400

    Checkbox test established and passing

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -7,25 +7,30 @@ describe("This is our first test!", () => {
 describe("Tests user inputs", () => {
     beforeEach(function () {
         // runs before each test in this block
-    cy.visit("http://localhost:3000")
+    cy.visit("http://localhost:3000");
     });
 
     it("Locates / tests the name input", () => {
         cy.get("#name") 
             .type("Ciomara Chavez de Oliver")
-            .should("have.value", "Ciomara Chavez de Oliver")
+            .should("have.value", "Ciomara Chavez de Oliver");
     });   
 
     it("Locates / tests the email input", () => {
         cy.get("#email")
             .type("cioma@mail.com")
-            .should("have.value", "cioma@mail.com")
+            .should("have.value", "cioma@mail.com");
     });
 
     it("Locates / tests the password input", () => {
         cy.get("#password")
             .type("abc123")
-            .should("have.value", "abc123")
+            .should("have.value", "abc123");
     });
 
+    it("Locates / tests the terms checkbox", () => {
+        cy.get("#terms")
+            .check()
+            .should("be.checked");
+    });
 });
