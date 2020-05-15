045869e92575e47a0525438423373acf322d18ae
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-05-14 20:33:07 -0400

    password test established and passing

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -8,19 +8,24 @@ describe("Tests user inputs", () => {
     beforeEach(function () {
         // runs before each test in this block
     cy.visit("http://localhost:3000")
-    })
+    });
 
     it("Locates / tests the name input", () => {
         cy.get("#name") 
             .type("Ciomara Chavez de Oliver")
             .should("have.value", "Ciomara Chavez de Oliver")
-    })    
+    });   
 
     it("Locates / tests the email input", () => {
         cy.get("#email")
             .type("cioma@mail.com")
             .should("have.value", "cioma@mail.com")
-    
+    });
+
+    it("Locates / tests the password input", () => {
+        cy.get("#password")
+            .type("abc123")
+            .should("have.value", "abc123")
     });
 
 });
